17cc481a9f28f35e63a759c85828a7dbeaae1fa7
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-01-30 12:48:20 +0200

    added UserController.java updated UserService.java

--- src/main/java/com/tkachuk/UserKeeper/service/UserService.java
+++ src/main/java/com/tkachuk/UserKeeper/service/UserService.java
@@ -2,12 +2,20 @@ package com.tkachuk.UserKeeper.service;
 
 import com.tkachuk.UserKeeper.model.User;
 import com.tkachuk.UserKeeper.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User findById(Long id) {
         return userRepository.getOne(id);
