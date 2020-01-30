2ebb0770c6c966a653e927844d708a4c88c213eb
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-01-30 12:33:52 +0200

    added User.java and UserRepository.java

---
+++ src/main/java/com/tkachuk/UserKeeper/model/User.java
@@ -0,0 +1,20 @@
+package com.tkachuk.UserKeeper.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+}
---
+++ src/main/java/com/tkachuk/UserKeeper/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.tkachuk.UserKeeper.repository;
+
+import com.tkachuk.UserKeeper.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
